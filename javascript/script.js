0a86b504804e9bb6f8b8a09429e6b2fe6dfc8aaf
evil-89789 <tngsurya055@gmail.com>
2024-03-24 19:36:44 +0545

    arrivals

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,37 @@
-// let navBar = document.querySelector('nav');
+let nextBtn = document.querySelector('.next');
+let prevBtn = document.querySelector('.prev');
+let slider = document.querySelector('.slider');
+let images = document.querySelectorAll('img');
 
-// window.onscroll=()=>{
-//     this.scrollY>10 ? navBar.style.position = "fixed" : navBar.style.position = "static";
-// }
+let slideNumNext = 1;
+
+let slideNumPrev = images.length;
+
+nextBtn.addEventListener('click',()=>{
+    slideNumNext < images.length ? slideNext() : firstSlide();
+})
+
+
+prevBtn.addEventListener('click',()=>{
+    slideNumNext > 1 ? slidePrev() : lastSlide ();
+})
+
+
+
+function slideNext() {
+        slider.style.transform = `translateX(-${slideNumNext * 100}%)`;
+        slideNumNext ++;
+}
+function slidePrev() {
+        slider.style.transform = `translateX(-${(slideNumNext-2) * 100}%)`;
+        slideNumNext --;
+}
+
+function firstSlide(){
+    slider.style.transform = `translateX(0)`;
+    slideNumNext = 1;
+}
+function lastSlide(){
+    slider.style.transform = `translateX(-${(images.length - 1)*100}%)`;
+    slideNumNext = 1;
+}
